b5ee025fc367b141afb21e9a6814998dff64dff4
Gabriel Augusto <gabrielaugustosb13@gmail.com>
2021-06-25 10:48:21 -0300

    starting code the middleware to know if the user can insert a tag

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express'
 import { CreateUserController } from './controllers/CreateUserController'
 import { CreateTagController } from './controllers/CreateTagController'
+import { ensureAdmin } from './middlewares/ensureAdmin'
 
 const router = Router()
 const createUserController = new CreateUserController()
 const createTagController = new CreateTagController()
 
+router.post("/tags", ensureAdmin, createTagController.handle) // esse método tem implicitamente request e response
 router.post("/users", createUserController.handle) // esse método tem implicitamente request e response
-router.post("/tags", createTagController.handle) // esse método tem implicitamente request e response
 
 export { router }
